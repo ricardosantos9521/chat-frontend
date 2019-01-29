968b09372be740a1dc98520b9d3231c5717be62b
Ricardo <ricardo.santos9521@outlook.com>
2019-01-29 19:19:57 +0000

    import service-worker and change worker

--- src/public/worker.js
+++ src/public/worker.js
@@ -1,20 +1,8 @@
-self.addEventListener('notificationclick', function (event) {
-    let url = 'https://rics.synology.me/signalr/client/';
-    event.notification.close(); // Android needs explicit close.
-    event.waitUntil(
-        clients.matchAll({ type: 'window' }).then(windowClients => {
-            // Check if there is already a window/tab open with the target URL
-            for (var i = 0; i < windowClients.length; i++) {
-                var client = windowClients[i];
-                // If so, just focus it.
-                if (client.url === url && 'focus' in client) {
-                    return client.focus();
-                }
-            }
-            // If not, then open the target URL in a new window/tab.
-            if (clients.openWindow) {
-                return clients.openWindow(url);
-            }
-        })
-    );
+importScripts("service-worker.js");
+
+self.addEventListener("message", (e) => {
+    if (e.data === "count") {
+        console.log("count in service worker");
+        self.postMessage("count");
+    }
 });
