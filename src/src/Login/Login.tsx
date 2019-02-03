b0a881cabec9529023a1457fdf125048ae9ced5b
Ricardo <ricardo.santos9521@outlook.com>
2019-02-03 22:17:47 +0000

    add routes to index and ui login base

---
+++ src/src/Login/Login.tsx
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+class Login extends Component {
+
+    constructor(props: Readonly<{}>) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <div>
+                Login Test
+            </div>
+        );
+    }
+}
+
+export default Login;
--- src/src/index.tsx
+++ src/src/index.tsx
@@ -3,8 +3,17 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { BrowserRouter, Route } from "react-router-dom";
+import Login from './Login/Login';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+    <BrowserRouter>
+        <div style={{ width: "100%", height: "100%" }}>
+            <Route exact path="/" component={App} />
+            <Route path="/login" component={Login} />
+        </div>
+    </BrowserRouter>
+    , document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
