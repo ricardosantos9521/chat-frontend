3d624bcca21d78e4f1ecb94a2f04f0f931cbd3c0
Ricardo <ricardo.santos9521@outlook.com>
2019-02-04 21:43:08 +0000

    add focus on parent notification (workarround to chrome)

--- src/public/worker.js
+++ src/public/worker.js
@@ -4,19 +4,23 @@ self.addEventListener("notificationclick", (event) => {
     let url = 'https://rics.synology.me/signalr/client/';
     var notification = event.notification;
     var action = event.action;
-    
+
     notification.close();
 
     event.waitUntil(
         clients.matchAll({ type: 'window' }).then(windowClients => {
             for (var i = 0; i < windowClients.length; i++) {
                 var client = windowClients[i];
-                if (client.url === url && 'focus' in client) {
-                    return client.focus();
+                if (client.url === url) {
+                    if ('parent' in client && 'focus' in client.parent) client.parent.focus();      //focus on parent chrome and recent browsers
+                    if ('focus' in client) client.focus();                                          //older browser
+                    return;
                 }
             }
+
             if (clients.openWindow) {
-                return clients.openWindow(url);
+                clients.openWindow(url);
+                return;
             }
         })
     );
