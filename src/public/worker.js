80a18171c25a644d4b02c10f3fcba5017d4fa0af
Ricardo Santos <ricardo.santos9521@outlook.com>
2019-11-18 12:56:43 +0000

    change path

--- src/public/worker.js
+++ src/public/worker.js
@@ -1,28 +1,28 @@
-importScripts("/chat/frontend/service-worker.js")
+// importScripts("/chat/frontend/service-worker.js")
 
-self.addEventListener("notificationclick", (event) => {
-    let url = 'https://rics.synology.me/chat/frontend/';
-    var notification = event.notification;
-    var action = event.action;
+// self.addEventListener("notificationclick", (event) => {
+//     let url = 'https://rics.synology.me/chat/frontend/';
+//     var notification = event.notification;
+//     var action = event.action;
 
-    notification.close();
+//     notification.close();
 
-    event.waitUntil(
-        clients.matchAll({ type: 'window' }).then(windowClients => {
-            for (var i = 0; i < windowClients.length; i++) {
-                var client = windowClients[i];
-                if (client.url === url) {
-                    if ('parent' in client && 'focus' in client.parent) client.parent.focus();      //focus on parent chrome and recent browsers
-                    if ('focus' in client) client.focus();                                          //older browser
-                    return;
-                }
-            }
+//     event.waitUntil(
+//         clients.matchAll({ type: 'window' }).then(windowClients => {
+//             for (var i = 0; i < windowClients.length; i++) {
+//                 var client = windowClients[i];
+//                 if (client.url === url) {
+//                     if ('parent' in client && 'focus' in client.parent) client.parent.focus();      //focus on parent chrome and recent browsers
+//                     if ('focus' in client) client.focus();                                          //older browser
+//                     return;
+//                 }
+//             }
 
-            if (clients.openWindow) {
-                clients.openWindow(url);
-                return;
-            }
-        })
-    );
-});
+//             if (clients.openWindow) {
+//                 clients.openWindow(url);
+//                 return;
+//             }
+//         })
+//     );
+// });
 
--- src/src/index.tsx
+++ src/src/index.tsx
@@ -7,7 +7,7 @@ import { BrowserRouter, Route } from "react-router-dom";
 import Login from './Login/Login';
 
 ReactDOM.render(
-    <BrowserRouter basename="/chat/frontend/">
+    <BrowserRouter basename="/">
         <div style={{ width: "100%", height: "100%" }}>
             <Route exact path="/" component={App} />
             <Route path="/login" component={Login} />
